84d606f7785f342c70d17c64c3a39c5dc3639a6d
NastasjaBakovic199 <nastasjabakovic199@gmail.com>
2023-02-01 00:08:54 +0100

    adde the join and the field mapping function

--- thats-life/src/thats_life/logic.cljs
+++ thats-life/src/thats_life/logic.cljs
@@ -13,7 +13,7 @@
 (def init-path
   (vec fields))
 
-(defn is-valid? [state]
+(defn is-valid? [game-state]
   (contains? state :players))
 
 (def guard -1)
@@ -24,3 +24,15 @@
 
 (defn initial-pawns [num-of-players]
   (nth [3 3 3 2 2] (- num-of-players 2)))
+
+(defn setup-fields [path]
+  (mapv
+    #(vec (remove nil? (conj %1 %2)))
+   (repeat (count path) nil)
+   (concat 
+    (repeat 8 nil)
+    (repeat 8 guard)
+    (repeat nil))))
+
+(defn join-game [game-state player-name]
+  (update state :players #(conj % name)))
