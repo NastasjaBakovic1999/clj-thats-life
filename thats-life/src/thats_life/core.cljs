2dfc9a2f31538f3cbfc17e45e76d1ddcfd069715
NastasjaBakovic199 <nastasjabakovic199@gmail.com>
2023-01-31 22:40:43 +0100

    started the implementation of rendering function

--- thats-life/src/thats_life/core.cljs
+++ thats-life/src/thats_life/core.cljs
@@ -5,8 +5,23 @@
 (defonce game-state
   (reagent/atom core/init-players :validator core/is-valid?))
 
+(def pawn-imgs [
+                "images/guard.svg"
+                "images/pawn-black.svg"
+                "images/pawn-blue.svg"
+                "images/pawn-green.svg"
+                "images/pawn-orange.svg"
+                "images/pawn-red.svg"
+                "images/pawn-yellow.svg" ])
+
+(defn game-render []
+  (let [state @game-state]
+    [:div 
+     [:h1 "That's life"]]))
+
 ;;The onbeforeunload event occurs when a document is about to be unloaded.
-;;This event allows you to display a message in a confirmation dialog box to inform the user 
-;;whether they wants to stay or leave the current page.
 (aset js/window "onbeforeunload" 
       (constantly "Do you really want to leave the game unfinished? Then you must lose!"))
+
+(reagent/render [game-render]
+                (js/document.querySelector "#game"))
